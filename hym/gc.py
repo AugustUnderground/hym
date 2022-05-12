5ed5c87fb7547000576c594af254557c0d445e66
Yannick Uhlmann <augustunderground@protonmail.com>
2022-05-12 08:45:31 +0200

    numpy data can't into json

--- hym/gc.py
+++ hym/gc.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-# __coconut_hash__ = 0x264acf72
+# __coconut_hash__ = 0x979e468b
 
 # Compiled with Coconut version 2.0.0-a_dev33 [How Not to Be Seen]
 
@@ -70,7 +70,8 @@ def make_env(env_id,  #type: str
      num  #type: int
     ):
     num_steps = 50
-    target_filter = ["a_0", "ugbw", "pm", "sr_r", "sr_f", "voff_stat", "cmrr", "psrr_p", "A"]
+    target_filter = ["a_0", "ugbw", "pm", "voff_stat", "cmrr", "psrr_p", "A"]
+#target_filter = ["ugbw", "pm", "A" , "voff_stat"] #, "a_0", "cmrr", "psrr_p"]
     env_name = "gace:{_coconut_format_0}-{_coconut_format_1}-v{_coconut_format_2}".format(_coconut_format_0=(env_id), _coconut_format_1=(backend), _coconut_format_2=(variant))
     env = (gym.make(env_name, target_filter=target_filter, max_steps=num_steps) if num == 1 else gace.vector_make_same(env_name, num, target_filter=target_filter, max_steps=num_steps))
     environment = Environment(env, env_name, env_id, backend, variant, num)
@@ -94,7 +95,7 @@ def predicate(env):
     return pred
 
 def scaler(env):
-    scl = (dict(((str(i)), (dict(((k), (p)) for k, p in gace.target.performance_scaler(e.ace_id, e.ace_backend).items() if k in e.target.keys()))) for i, e in (enumerate)(env.env)) if env.num > 1 else {"0": dict(((k), (p)) for k, p in gace.target.performance_scaler(env.ace_id, env.ace_backend).items() if k in env.env.target.keys())})
+    scl = (dict(((str(i)), (gace.target.performance_scaler(e.ace_id, e.ace_backend))) for i, e in (enumerate)(env.env)) if env.num > 1 else {"0": gace.target.performance_scaler(env.ace_id, env.ace_backend)})
 
     return scl
 
@@ -129,7 +130,7 @@ def close(env):
     return closed
 
 def current_performance(env):
-    perf = (dict(((str(i)), (dict(((k), ((np.nan_to_num)(v))) for k, v in ((ac.current_performance)(e.ace)).items()))) for i, e in (enumerate)(env.env)) if env.num > 1 else {"0": dict(((k), ((np.nan_to_num)(v))) for k, v in ((ac.current_performance)(env.env.ace)).items())})
+    perf = (dict(((str(i)), (dict(((k), (((np.nan_to_num)(v)).item())) for k, v in ((ac.current_performance)(e.ace)).items()))) for i, e in (enumerate)(env.env)) if env.num > 1 else {"0": dict(((k), ((np.nan_to_num)(v))) for k, v in ((ac.current_performance)(env.env.ace)).items())})
 
     return perf
 
